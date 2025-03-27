585afed6856ece5c2ce06fcbd4d32e692c580f63
NikolaCerna <nikolacerna02@gmail.com>
2025-03-27 22:08:28 +0100

    rozdelenie nav a headeru
    zmena rozdelenia

--- parts/header.php
+++ parts/header.php
@@ -1,16 +1,12 @@
-<header class="placeholder">
-			<div class="parallax-window" data-parallax="scroll" data-image-src="img/simple-house-01.jpg">
-				<div class="tm-header">
-					<div class="row tm-header-inner">
-						<div class="col-md-6 col-12">
-							<img src="img/simple-house-logo.png" alt="Logo" class="tm-site-logo" />
-							<div class="tm-site-text-box">
-								<h1 class="tm-site-title">Simple House</h1>
-								<h6 class="tm-site-description">new restaurant template</h6>
-							</div>
-						</div>
-                        <?php include "parts/nav.php" ?>
-					</div>
-				</div>
-			</div>
-</header>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Moja stránka</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400" rel="stylesheet" />
+    <link href="css/templatemo-style.css" rel="stylesheet" />
+</head>
+
