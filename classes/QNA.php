851f8e8b5fadebb20c235b7ffebebaf050895c96
NikolaCerna <nikolacerna02@gmail.com>
2025-05-08 15:43:17 +0200

    pridana class qna

---
+++ classes/QNA.php
@@ -0,0 +1,50 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', "On");
+require_once(__ROOT__.'/classes/Database.php');
+
+class QNA extends Database {
+    protected $connection;
+
+    public function __construct() {
+        $this->connect();
+        $this->connection = $this->getConnection();
+    }
+
+    public function insertQna(string $otazky, string $odpovede): bool
+    {
+        $sql = "INSERT INTO faq(otazka, odpoved) VALUES ('" . $otazky . "', '" . $odpovede . "')";
+        $stmt = $this->connection->prepare($sql);
+        return $stmt->execute();
+    }
+
+    public function getQna(): array
+    {
+        $sql = "SELECT * FROM faq";
+        $query = $this->connection->query($sql);
+        $data = $query->fetchAll(\PDO::FETCH_ASSOC);
+        $finalQna = [];
+
+        foreach ($data as $item) {
+            $ID = $item['ID'];
+            $otazka = $item['otazka'];
+            $odpoved = $item['odpoved'];
+            $finalQna[$ID] = [
+                'ID' => $ID,
+                'otazka' => $otazka,
+                'odpoved' => $odpoved
+            ];
+        }
+        return $finalQna;
+    }
+
+    public function deleteQna(int $ID): bool
+    {
+        $sql = "DELETE FROM faq WHERE ID = " . $ID;
+        $stmt = $this->connection->prepare($sql);
+        return $stmt->execute();
+    }
+
+
+
+}
